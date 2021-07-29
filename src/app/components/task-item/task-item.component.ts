4193e1968a8f6a2135f500355063e5831e3ecab8
dmiloloza <domagoj.miloloza@gmail.com>
2021-07-29 14:38:40 +0200

    red remove button added to task-item component

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {faTimes} from '@fortawesome/free-solid-svg-icons';
 import {Task} from '../../Task';
 
 @Component({
@@ -8,6 +9,7 @@ import {Task} from '../../Task';
 })
 export class TaskItemComponent implements OnInit {
   @Input() task!: Task;
+  faTimes = faTimes;
 
   constructor() {
   }
